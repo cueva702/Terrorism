b886c37dc4a92b772aaddeba34fe3ae1e4253a23
kresta6 <kresta674@gmail.com>
2017-12-03 16:01:49 -0800

    I completed Shiny App maps of terorrist attacks in USE from 1970 to 2016. I chose USA because doing entire world was too much data. The shiny app maps is in folder terrormap

---
+++ terrormap/YearUSATerrordata.R
@@ -0,0 +1,19 @@
+library(ggmap)
+library(magrittr)
+library(maps)
+library(mapproj)
+library(ggplot2)
+library(plyr)
+library(dplyr)
+library(htmltools)
+library(htmlwidgets)
+
+usaTerror20002016 <- read.csv('USATerrorists20002016.csv')
+View(usaTerror20002016)
+
+usaTerror19991983 <- read.csv('19831999USATerrorAttacks.csv')
+View(usaTerror19991983)
+
+usaTerror19701982 <- read.csv('19701982USATerrorAttacks.csv')
+View(usaTerror19701982)
+
---
+++ terrormap/app.R
@@ -0,0 +1,55 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+library(leaflet)
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Terrorist Attacks in the USA from 1970-2016"),
+  
+  
+  
+  
+  
+  # Show a plot of the generated distribution
+  mainPanel(
+    tabsetPanel(type = "tabs", 
+                tabPanel('Terrorist attacks in USA 2000-2016 Map', leafletOutput("mymap")),
+                tabPanel('Terrorist Attacks in USA 1983-1999 Map', leafletOutput("usa8090_map")),
+                tabPanel('Terrorist Attacks in USA 1970-1982 Map', leafletOutput("usa7080_map"))
+    )))
+
+
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+  
+  output$mymap <- renderLeaflet({
+    leaflet(data = usaTerror20002016) %>% addTiles() %>%
+      addMarkers(~longitude, ~latitude, popup = ~as.character(summary), label = ~as.character(city))
+    
+  })
+  
+  output$usa8090_map <- renderLeaflet({
+    leaflet(data = usaTerror19991983) %>% addTiles() %>%
+      addMarkers(~longitude, ~latitude, popup = ~as.character(summary), label = ~as.character(city))
+  })
+  
+  output$usa7080_map <- renderLeaflet({
+    leaflet(data = usaTerror19701982) %>% addTiles() %>%
+      addMarkers(~longitude, ~latitude, popup = ~as.character(summary), label = ~as.character(city))
+  }) 
+  
+}
+
+shinyApp(ui, server)
+
